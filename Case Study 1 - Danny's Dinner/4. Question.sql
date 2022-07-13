904afbc5c990a850919a2d2b6268bfc31065975f
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-07-13 23:18:10 +0530

    Create 4. Question.sql

---
+++ Case Study 1 - Danny's Dinner/4. Question.sql
@@ -0,0 +1,16 @@
+-- -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+WITH Max_orders AS(
+				SELECT s.product_id, 
+					   COUNT(s.product_id) AS Total_orders
+				FROM   sales s
+				GROUP BY s.product_id)
+SELECT 
+    M.product_name, MAX(T.total_orders)
+FROM
+    Max_orders T
+        JOIN
+    menu M ON M.product_id = T.product_id
+GROUP BY M.product_name
+ORDER BY T.total_orders DESC
+LIMIT 1;
