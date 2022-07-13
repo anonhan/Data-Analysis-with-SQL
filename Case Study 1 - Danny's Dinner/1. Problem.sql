9b3e22cafa72d5fc7b173192e2996b80fb7229df
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-07-13 22:58:40 +0530

    Create 1. Problem.sql

---
+++ Case Study 1 - Danny's Dinner/1. Problem.sql
@@ -0,0 +1,13 @@
+-- What is the total amount each customer spent at the restaurant?
+
+WITH amt_spent AS
+			(SELECT 	S.customer_id, 
+						S.product_id, 
+						SUM(CASE WHEN s.product_id IS NOT NULL THEN 1 ELSE 0 END) as Total_Orders   
+			FROM 		sales S
+            GROUP BY 	S.customer_id, S.product_id
+            )
+SELECT 		A.customer_id,
+			SUM(A.Total_Orders * price) 'Total Amount Spent' 
+FROM		amt_spent AS A JOIN menu M ON M.product_id = A.product_id 
+GROUP BY	A.customer_id;
