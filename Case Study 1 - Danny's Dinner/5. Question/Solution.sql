25fd64f1e1e75e4dff365779522d46043ba3f0e7
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-07-17 22:50:58 +0530

    Update Solution.sql

--- Case Study 1 - Danny's Dinner/5. Question/Solution.sql
+++ Case Study 1 - Danny's Dinner/5. Question/Solution.sql
@@ -7,14 +7,14 @@ FROm   		sales s
 GROUP BY 	s.customer_id , s.product_id;
 
 WITH Popular_items AS (
-		SELECT	P.customer_id,
-				P.product_id,
-                P.freq,
-                ROW_NUMBER() OVER (PARTITION BY customer_id ORDER By freq DESC) AS ranks
-        FROM	Products_Bought P
+	SELECT	P.customer_id,
+		P.product_id,
+		P.freq,
+		ROW_NUMBER() OVER (PARTITION BY customer_id ORDER By freq DESC) AS ranks
+	FROM	Products_Bought P
         )
-		SELECT 	customer_id,
-				product_iD,
-				freq AS Total_Orders
-		FROM 	Popular_items
-		WHERE	ranks = 1;
+SELECT 	customer_id,
+	product_iD,
+	freq AS Total_Orders
+FROM 	Popular_items
+WHERE	ranks = 1;
