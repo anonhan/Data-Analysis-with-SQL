8b56c635629c5f0fe9e41dad99dc8a66ace725fe
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-07-17 22:49:40 +0530

    Update Solution.sql

--- Case Study 1 - Danny's Dinner/5. Question/Solution.sql
+++ Case Study 1 - Danny's Dinner/5. Question/Solution.sql
@@ -1,6 +1,11 @@
 -- Step 1: First select that how many times each item has been bought by each customer using group by
 -- Step 2: assigning rank to each subgroup in desc order by Total Orders (freq)
 
+CREATE TEMPORARY TABLE Products_Bought
+SELECT 		s.customer_id, s.product_id, COUNT(s.product_id) as freq
+FROm   		sales s
+GROUP BY 	s.customer_id , s.product_id;
+
 WITH Popular_items AS (
 		SELECT	P.customer_id,
 				P.product_id,
